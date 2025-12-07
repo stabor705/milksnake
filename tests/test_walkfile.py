4b66e27ab82c15e776e75874f32c67e7527952b0
Stanisław Borowy <stabor@startmail.com>
2025-12-07 19:04:10 +0100

    handle walkfiles

---
+++ tests/test_walkfile.py
@@ -0,0 +1,48 @@
+def test_parse_regular_line():
+    from milksnake.walkfile import _parse_line, VariableBindingEntry
+
+    line = ".1.3.6.1.2.1.2.2.1.4.4 = INTEGER: 1500"
+    entry = _parse_line(line)
+
+    assert isinstance(entry, VariableBindingEntry)
+    assert entry.oid == "1.3.6.1.2.1.2.2.1.4.4"
+    assert entry.type == "INTEGER"
+    assert entry.value == "1500"
+
+def test_parse_null_line():
+    from milksnake.walkfile import _parse_line, NullEntry
+
+    line = ".1.3.6.1.2.1.2.2.1.4.4 = \"\""
+    entry = _parse_line(line)
+    assert isinstance(entry, NullEntry)
+    assert entry.oid == "1.3.6.1.2.1.2.2.1.4.4"
+
+def test_parse_empty_value():
+    from milksnake.walkfile import _parse_line, VariableBindingEntry
+
+    line = ".1.3.6.1.2.1.2.2.1.4.4 = STRING: "
+    entry = _parse_line(line)
+    assert isinstance(entry, VariableBindingEntry)
+    assert entry.oid == "1.3.6.1.2.1.2.2.1.4.4"
+    assert entry.type == "STRING"
+    assert entry.value == ""
+
+def test_parse_walkfile():
+    from io import StringIO
+    from milksnake.walkfile import parse_walkfile, VariableBindingEntry, NullEntry
+    file_mock = StringIO(""".1.3.6.1.2.1.2.2.1.4.4 = INTEGER: 1500
+.1.3.6.1.2.1.2.2.1.4.5 = \"\"
+.1.3.6.1.2.1.2.2.1.4.6 = STRING: 
+""")
+    entries = list(parse_walkfile(file_mock))
+    assert len(entries) == 3
+    assert isinstance(entries[0], VariableBindingEntry)
+    assert entries[0].oid == "1.3.6.1.2.1.2.2.1.4.4"
+    assert entries[0].type == "INTEGER"
+    assert entries[0].value == "1500"
+    assert isinstance(entries[1], NullEntry)
+    assert entries[1].oid == "1.3.6.1.2.1.2.2.1.4.5"
+    assert isinstance(entries[2], VariableBindingEntry)
+    assert entries[2].oid == "1.3.6.1.2.1.2.2.1.4.6"
+    assert entries[2].type == "STRING"
+    assert entries[2].value == ""
